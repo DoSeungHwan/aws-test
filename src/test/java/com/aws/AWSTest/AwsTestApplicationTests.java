e8206b50839bcb3136db3c22172ca1de91cff7e8
Do SeungHwan <aim970126@gmail.com>
2023-02-03 14:03:35 +0900

    Add files via upload

---
+++ src/test/java/com/aws/AWSTest/AwsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.AWSTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
